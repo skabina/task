87054c9fe2b03425b036530e076e133ac928a684
Borch Andrii <skabinausa@gmail.com>
2025-06-09 21:35:55 +0300

    First day

---
+++ README.md
@@ -0,0 +1,8 @@
+# Telegram Analyzer
+
+##  Встановлення
+
+```bash
+python -m venv venv
+source venv/bin/activate
+pip install .
---
+++ src/main.py
@@ -0,0 +1,14 @@
+import os
+from getDate import get_Date
+from telethon import TelegramClient
+from dotenv import load_dotenv
+
+
+
+
+def main():
+    print("Starting the Telegram client...")
+    # ...
+    
+if __name__ == "__main__":
+    main()
