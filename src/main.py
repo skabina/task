1e09c77c5bafa27a3857446d2123ab677e5c60ff
Borch Andrii <skabinausa@gmail.com>
2025-06-10 22:12:03 +0300

    Зміни

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import os
 from dotenv import load_dotenv
 from telethon import TelegramClient
-from getDate import get_Date, client
+from get_date import get_date, client
 import asyncio
 
 
 async def main():
     
     await client.start() 
-    await get_Date(client)
+    await get_date(client)
     await client.disconnect()
 
 if __name__ == "__main__":
