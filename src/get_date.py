c1ffbc78a70613d6f0e6c74c8373df95ff07451a
Borch Andrii <skabinausa@gmail.com>
2025-06-11 12:33:21 +0300

    Sort messages by time

--- src/get_date.py
+++ src/get_date.py
@@ -1,44 +1,65 @@
 import json
 import os
+import asyncio
 from dotenv import load_dotenv
 from telethon import TelegramClient
 from datetime import datetime, timedelta, timezone
+from telethon.errors import FloodWaitError
 
 load_dotenv()
-
 
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 
-
 client = TelegramClient('date', api_id, api_hash)
 
-
-one_month_ago = datetime.now(timezone.utc) - timedelta(month=1)  
-
+one_month_ago = datetime.now(timezone.utc) - timedelta(days=1)  # За останній день
 
 async def get_date(client):
- 
     chats_data = []
-    
+
     async for dialog in client.iter_dialogs(limit=10):
         chat_info = {"chat_id": dialog.id, "chat_name": dialog.name, "messages": []}
 
         if dialog.is_group or dialog.is_user:
             print(f"\nЧат: {dialog.name} — ID: {dialog.id}")
-            
-            
+
+            messages = []
             async for message in client.iter_messages(dialog.id):
                 if message.date < one_month_ago:
-                    break  
+                    break
                 if message.text:
-                    sender = await client.get_entity(message.sender_id)
-                    sender_name = sender.username if sender.username else str(sender.id)
-                    chat_info["messages"].append(f"{sender_name}: {message.text[:50]}")
-                    chats_data.append(chat_info)
+                    try:
+                        sender = await client.get_entity(message.sender_id)
+                        sender_name = sender.username if sender.username else str(sender.id)
+                        msg_time = message.date.astimezone(timezone.utc).strftime("%H:%M")
+                        messages.append({"date": message.date, "text": f"{sender_name} [{msg_time}]: {message.text[:50]}"})
+                    except FloodWaitError as e:
+                       
+                        print(f"FloodWaitError: Очікуємо {e.seconds} секунд.")
+                        await asyncio.sleep(e.seconds)  
+                        continue  
+
+            # Відсортувати повідомлення за датою
+            messages.sort(key=lambda x: x["date"])
+
+        
+            chat_info["messages"] = [msg["text"] for msg in messages]
+
+            if chat_info["messages"]: 
+                chats_data.append(chat_info)
 
         else:
             print(f"\nПропускаємо канал: {dialog.name} — ID: {dialog.id}")
-            
+
+   
     with open('chats_data.json', 'w', encoding='utf-8') as json_file:
         json.dump(chats_data, json_file, ensure_ascii=False, indent=4)
+
+
+async def main():
+    await client.start()
+    await get_date(client)
+
+if __name__ == "__main__":
+    asyncio.run(main())
