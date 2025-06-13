bfde4d96db6f1e880a880d29721784240872f66c
Borch Andrii <skabinausa@gmail.com>
2025-06-13 22:15:47 +0300

    Revision get_date for class

--- src/get_date.py
+++ src/get_date.py
@@ -6,51 +6,58 @@ from telethon import TelegramClient
 from datetime import datetime, timedelta, timezone
 from telethon.errors import FloodWaitError
 from config import api_id, api_hash
+import logging
 
-client = TelegramClient('date', api_id, api_hash)
+logging.basicConfig(level = logging.DEBUG)
 
-one_month_ago = datetime.now(timezone.utc) - timedelta(days=30)  # За останній день
 
-async def get_date(client):
-    chats_data = []
+one_month_ago = datetime.now(timezone.utc) - timedelta(days=1)  
 
-    async for dialog in client.iter_dialogs(limit=10):
-        chat_info = {"chat_id": dialog.id, "chat_name": dialog.name, "messages": []}
 
-        if dialog.is_group or dialog.is_user:
-            print(f"\nЧат: {dialog.name} — ID: {dialog.id}")
 
-            messages = []
-            async for message in client.iter_messages(dialog.id):
-                if message.date < one_month_ago:
-                    break
-                if message.text:
-                    try:
-                        sender = await client.get_entity(message.sender_id)
+class TelegramAccount():
+    def __init__ (self, ):
+        self.client = TelegramClient('date', api_id, api_hash)
+
+    def start(self):
+        return self.client.start()
+    
+    def disconnect(self):
+        return self.client.disconnect()
+
+    async def get_date(self):
+        chats_data = []
+
+        async for dialog in self.client.iter_dialogs(limit=10):
+            chat_info = {
+                "chat_id": dialog.id, 
+                "chat_name": dialog.name, 
+                "messages": []
+            }
+
+            if dialog.is_group or dialog.is_user:
+                logging.info( u'Everything is cool')
+                
+                messages = []
+                async for message in self.client.iter_messages(dialog.id):
+                    if message.date < one_month_ago:
+                        logging.info(f"Skipping message from {message.date} as it is older than one month.")
+                        break
+                    if message.text:
+                        sender = await self.client.get_entity(message.sender_id)
                         sender_name = sender.username if sender.username else str(sender.id)
                         msg_time = message.date.astimezone(timezone.utc).strftime("%H:%M")
                         messages.append({"date": message.date, "text": f"{sender_name} [{msg_time}]: {message.text[:50]}"})
-                    except FloodWaitError as e:
-                       
-                        print(f"FloodWaitError: Очікуємо {e.seconds} секунд.")
-                        await asyncio.sleep(e.seconds)  
-                        continue  
 
-            messages.sort(key=lambda x: x["date"])    
-            chat_info["messages"] = [msg["text"] for msg in messages]
+                            
+                messages.sort(key=lambda x: x["date"])    
+                chat_info["messages"] = [msg["text"] for msg in messages]
 
-            if chat_info["messages"]: 
-                chats_data.append(chat_info)
-
-        else:
-            print(f"\nПропускаємо канал: {dialog.name} — ID: {dialog.id}")
-   
-    with open('chats_data.json', 'w', encoding='utf-8') as json_file:
-        json.dump(chats_data, json_file, ensure_ascii=False, indent=4)
-
-async def main():
-    await client.start()
-    await get_date(client)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+                if chat_info["messages"]: 
+                    chats_data.append(chat_info)         
+            else:
+                logging.info(f"Skipping non-user chat: {dialog.name} (ID: {dialog.id})")
+    
+        with open('chats_data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(chats_data, json_file, ensure_ascii=False, indent=4)
+        logging.info(f"Saved {len(chats_data)} chats to 'chats_data.json'.")
